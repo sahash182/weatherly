785d337a6550f632259117cfb33b35c0e8732bfc
Sahash <sahash182@gmail.com>
2015-10-09 11:36:32 -0700

    error handling added

--- app.js
+++ app.js
@@ -24,13 +24,15 @@ angular.module('weatherly', ['ngRoute'])
     $scope.searchCity = function () {
       var city = $scope.city.replace(/\s+/, '');
       $scope.currentCity = $scope.city;
-      var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?&q="  + city + "&type=accurate&,us&mode=json&callback=JSON_CALLBACK";
+      var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast?&q="  + city + "&type=accurate&,us&mode=json&callback=JSON_CALLBACK&APPID=8aa25b237192dd69078ca44e1b1e2598";
         $http.jsonp(weatherUrl)
         .then(function (response) {
           //error handling if city  not found or no response
           if (response == undefined || response == null){
             alert("No City Found!");
-          }else{
+          }else if (response.status !== 400{
+            alert("Mayday! Mayday! The server is down.")
+          } else {
             $scope.city = '';
             $scope.weather = response.data;
           };
